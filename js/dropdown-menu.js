d87c9274e6d848bc8fadb145819e273678e07598
joneilson <joneilson84@gmail.com>
2022-11-22 00:54:51 -0300

    update project

---
+++ js/dropdown-menu.js
@@ -0,0 +1,21 @@
+const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+
+dropdownMenus.forEach(menu => {
+    ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick);
+    })
+})
+
+function handleClick(event) {
+    event.preventDefault();
+    this.classList.toggle('active');
+    outsideClick();
+}
+
+function outsideClick() {
+    const html = document.documentElement;
+    html.addEventListener('click', handleOutsideClick);
+    function handleOutsideClick(event) {
+        console.log(event);
+    }
+}
---
+++ js/menu-mobile.js
@@ -0,0 +1,13 @@
+function initMenuMobile() {
+    const menuButton = document.querySelector('[data-menu="button"]');
+    const menuList = document.querySelector('[data-menu="list"]');
+    
+    function openMenu(event) {
+        menuList.classList.toggle('active');
+        menuButton.classList.toggle('active');
+    
+    }
+    
+    menuButton.addEventListener('click', openMenu);
+}
+initMenuMobile();
